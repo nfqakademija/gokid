b725c4942c9471963e298e05423ff374638cf19e
Žygimantas Rukas <zyrukas@gmail.com>
2016-03-22 16:58:58 +0200

    FOSUserBundle integration.

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * User constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
