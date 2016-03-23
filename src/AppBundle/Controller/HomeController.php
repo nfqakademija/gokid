7d0bff366a8022fc3455f89c9baff9098f1acb42
Rimantas Kabaila <rimas.kabaila@gmail.com>
2016-03-23 03:08:30 +0200

    Input & output to test search

--- src/AppBundle/Controller/HomeController.php
+++ src/AppBundle/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Repository\OfferRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class HomeController
@@ -18,5 +20,21 @@ class HomeController extends Controller
     public function indexAction()
     {
         return $this->render('AppBundle:Home:index.html.twig', array());
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function searchAction(Request $request)
+    {
+//        var_dump(get_loaded_extensions());die();
+        /** @var OfferRepository $offerRepository */
+        $offerRepository = $this->getDoctrine()->getRepository('AppBundle:Offer');
+
+        return $this->render('AppBundle:Home:search.html.twig', [
+            'offers' => $offerRepository->search($request),
+            'modules' => get_loaded_extensions(),
+        ]);
     }
 }
