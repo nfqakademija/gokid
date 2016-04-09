47df98586d5b144ba6275390bbea2b16745d7ab6
Lukas Kriaučiūnas <lukaskriauciunas@outlook.com>
2016-04-09 22:25:49 +0300

    Index search page revamp

--- src/AppBundle/Form/IndexSearchOffer.php
+++ src/AppBundle/Form/IndexSearchOffer.php
@@ -6,6 +6,9 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
 
 class IndexSearchOffer extends AbstractType
 {
@@ -19,7 +22,24 @@ class IndexSearchOffer extends AbstractType
             'label' => false,
             'attr' => [
                 'placeholder' => 'Gyvenamoji vieta',
+                'class' => 'place-input',
             ],
+        ])->add('ageFrom', IntegerType::class, [
+            'label' => false,
+            'attr' => [
+                'placeholder' => 'Vaiko amžius',
+                'class' => 'age-input'
+            ],
+        ])->add('male', CheckboxType::class, [
+            'label' => false,
+            'attr' => [
+                'class' => 'gender-checkbox',
+            ]
+        ])->add('female', CheckboxType::class, [
+            'label' => false,
+            'attr' => [
+                'class' => 'gender-checkbox',
+            ]
         ]);
     }
     
