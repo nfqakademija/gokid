93735622053da1ae9d0352f64aabdf7fa6ae7177
Lukas Kriaučiūnas <lukaskriauciunas@outlook.com>
2016-04-18 22:22:30 +0300

    Added UserBundle to override default FOSUserBundle registration action

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
+            new \UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
