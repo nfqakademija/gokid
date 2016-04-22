537c498289078b0699ab9f4ef950bc02843af882
Rimantas Kabaila <rimas.kabaila@gmail.com>
2016-04-22 18:37:25 +0300

    Added entity Comment

--- src/AppBundle/Entity/Comment.php
+++ src/AppBundle/Entity/Comment.php
@@ -9,14 +9,12 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use FOS\CommentBundle\Entity\Comment as BaseComment;
 
 /**
  * @ORM\Table(name="comments")
  * @ORM\Entity
- * @ORM\ChangeTrackingPolicy("DEFERRED_EXPLICIT")
  */
-class Comment extends BaseComment
+class Comment
 {
     /**
      * @ORM\Id
@@ -26,10 +24,163 @@ class Comment extends BaseComment
     protected $id;
 
     /**
-     * Thread of this comment
+     * @var string
      *
-     * @var Thread
-     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Thread")
+     * @ORM\Column(name="title", type="string", length=45)
      */
-    protected $thread;
+    protected $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="body", type="text")
+     */
+    protected $body;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=45)
+     */
+    protected $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=45)
+     */
+    protected $email;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="rate", type="integer")
+     */
+    protected $rate;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Offer", inversedBy="comments")
+     * @ORM\JoinColumn(name="offer_id", referencedColumnName="id")
+     */
+    protected $offer;
+
+    /**
+     * Set offer
+     *
+     * @param Offer $offer
+     *
+     * @return Comment
+     */
+    public function setOffer(Offer $offer = null)
+    {
+        $this->offer = $offer;
+
+        return $this;
+    }
+
+    /**
+     * Get offer
+     *
+     * @return Offer
+     */
+    public function getOffer()
+    {
+        return $this->offer;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRate()
+    {
+        return $this->rate;
+    }
+
+    /**
+     * @param mixed $rate
+     */
+    public function setRate($rate)
+    {
+        $this->rate = $rate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
 }
