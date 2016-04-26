9090486df912205e74d01c3baabd89de6b2b3302
Žygimantas Rukas <zyrukas@gmail.com>
2016-04-26 16:23:33 +0300

    UE improvement

--- app/Resources/scripts/offers.js
+++ app/Resources/scripts/offers.js
@@ -106,13 +106,13 @@ function clearMarkers() {
 function ajaxUpdate(page) {
     var url = rootUrl + "search?address="+$('#address').val()+(($('#male').is(':checked')) ? '&male=1' : '')+(($('#female').is(':checked')) ? '&female=1' : '')+"&age="+$('#age').val()+"&latitude="+$('#latitude').val()+"&longitude="+$('#longitude').val()+"&distance="+$('#distance').val()+((page > 0) ? '&page='+page : '');
 
+    history.pushState(null, null, url);
+    
     $.get( url+"&ajax=1", function( data ) {
         clearMarkers();
 
         $('.offer-objects').fadeOut('fast');
         $('.offer-objects').html(data).fadeIn('fast');
-
-        history.pushState(null, null, url);
     });
 }
 
