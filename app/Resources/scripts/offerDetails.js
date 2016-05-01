cf27504d91f43ed0f8ec19109e7dc46746f34173
Žygimantas Rukas <zyrukas@gmail.com>
2016-05-02 00:07:08 +0300

    reviews

--- app/Resources/scripts/offerDetails.js
+++ app/Resources/scripts/offerDetails.js
@@ -15,3 +15,19 @@ $(".owl-carousel").owlCarousel({
         }
     }
 });
+
+function detailsMap(lat, lng) {
+    map = new google.maps.Map(document.getElementById('map-details'), {
+        zoom: 15,
+        center: {lat: lat, lng: lng},
+        scrollwheel: false,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        }
+    });
+    marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map,
+        icon: green,
+    });
+}
