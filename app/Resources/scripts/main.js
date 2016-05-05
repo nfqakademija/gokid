d81ca62f09545c14f6992d848ed4b7bcb8496b76
Žygimantas Rukas <zyrukas@gmail.com>
2016-05-05 16:33:14 +0300

    gender selection

--- app/Resources/scripts/main.js
+++ app/Resources/scripts/main.js
@@ -2,3 +2,11 @@ $(".offer-count.counter").counterUp({
     delay: 10,
     time: 1000
 });
+
+/**
+ * Toggle gender buttons
+ */
+$('.gender-input button').click(function() {
+    $(this).toggleClass('active');
+    $('#'+$(this).attr('for')).click();
+});
