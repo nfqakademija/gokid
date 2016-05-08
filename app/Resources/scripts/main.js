1e032977f6b40bde32c8cd102be331cd1ca15bae
Lukas Kriaučiūnas <lukaskriauciunas@outlook.com>
2016-05-08 13:38:47 +0300

    Gender buttons changed

--- app/Resources/scripts/main.js
+++ app/Resources/scripts/main.js
@@ -1,12 +1,36 @@
-$(".offer-count.counter").counterUp({
-    delay: 10,
-    time: 1000
-});
+$(document).ready(function() {
 
-/**
- * Toggle gender buttons
- */
-$('.gender-input button').click(function() {
-    $(this).toggleClass('active');
-    $('#'+$(this).attr('for')).click();
+    $(".offer-count.counter").counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+    /**
+     * Toggle gender buttons
+     */
+    $('.gender-input button').click(function() {
+        $(this).toggleClass('active');
+        $('#'+$(this).attr('for')).click();
+    });
+
+    /**
+     * Set gender checkbox when button clicked
+     */
+    $('.male').click(function() {
+        $('male-checkbox').click();
+    });
+
+    $('.female').click(function() {
+        $('female-checkbox').click();
+    });
+
+    if ($('.male-checkbox').is(':checked')) {
+        $('.male').addClass('active');
+        console.log('1');
+    }
+
+    if ($('.female-checkbox').is(':checked')) {
+        $('.female').addClass('active');
+        console.log('2');
+    }
 });
